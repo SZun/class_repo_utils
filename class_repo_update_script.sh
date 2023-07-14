e88353ad5d25614feffc6ad960c75ba31c2691c4
Sam Zun <zunsamuel97@gmail.com>
2023-07-14 18:03:52 -0500

    successfully copying homework and deleting solution

--- class_repo_update_script.sh
+++ class_repo_update_script.sh
@@ -11,7 +11,7 @@ master_repo_name="DataViz-Lesson-Plans/"
 lesson_directory="01-Lesson-Plans/"
 homework_directory="02-Homework/"
 canvas_directory="04-Canvas/"
-week_directory="09-SQL/"
+week_directory="09-SQL"
 day="1"
 is_solutions=false
 
@@ -28,70 +28,44 @@ master_repo_directory="$root_directory$master_repo_name"
 master_lesson_day_directory="$master_repo_directory$lesson_week_directory$day"
 
 
-# Change directory into your local class repo
-cd $class_repo_directory
+# Change directory into your local class repo and pull from main branch
+cd $class_repo_directory && git pull origin main
 
-#Pull current version of main
-git pull origin main
 
 # Check if lesson directory deos not exists
 if [ ! -d "$class_lesson_week_directory" ]; 
 then
+
     # Create lesson directory
-    mkdir $class_lesson_week_directory
+    # mkdir $class_lesson_week_directory
 
     # Copy/Paste current week homework directory
-    cp -r "${master_repo_directory}${homework_directory}${week_directory}" $class_homework_directory
+    cp -r "$master_repo_directory$homework_directory$week_directory" $class_homework_directory
     # Delete homework solution directory
-    rm -rf "${class_homework_directory}${week_directory}Solutio**"
-    
+    rm -rf "${class_homework_directory}${week_directory}/"Solutio*
+
     # Copy/Paste current week Canvas directory
-    cp -r "${master_repo_directory}${canvas_directory}${week_directory}" "${class_repo_directory}${canvas_directory}${week_directory}"
+    cp -r "$master_repo_directory$canvas_directory$week_directory" "$class_repo_directory$canvas_directory$week_directory"
 fi
 
-# Check if adding solutions
-if [ "$is_solutions" = true ]; 
-then
-    # Delete current day directory
-    rm -rf "${class_lesson_day_directory}"
-fi
+# # Check if adding solutions
+# if [ "$is_solutions" = true ]; 
+# then
+#     # Delete current day directory
+#     rm -rf "${class_lesson_day_directory}"
+# fi
 
-# Copy/Paste folder
-cp -r $master_lesson_day_directory $class_lesson_week_directory
+# # Copy/Paste folder
+# cp -r $master_lesson_day_directory $class_lesson_week_directory
 
-# Check if not adding solutions
-if [ "$is_solutions" = false ]; 
-then
-    # Delete student student solved folders
-    rm -rf "${class_lesson_day_directory}/Activities/**Stu**/Solved"
-fi
+# # Check if not adding solutions
+# if [ "$is_solutions" = false ]; 
+# then
+#     # Delete student student solved folders
+#     rm -rf "${class_lesson_day_directory}/Activities/**Stu**/Solved"
+# fi
 
-# Delete LessonPlan.md file
-rm -rf "${class_lesson_day_directory}/LessonPlan.md"
+# # Delete LessonPlan.md file
+# rm -rf "${class_lesson_day_directory}/LessonPlan.md"
 
-# Add changes
-git add -A
-
-# Set commit message
-message="${week} Day ${day} Content"
-if [ "$is_solutions" = true ]; 
-then
-    message="${message} Solutions"
-fi
-# Commit changes
-git commit -m "${message}"
-
-# Push changes
-# git push -u origin script_testing
-
-# Change directory back into DataViz-Lesson-Plans
-cd "${master_repo_directory}"
-
-# Add changes
-git add -A
-
-# Stash changes
-git stash
-
-# Drop stashed changes
-git stash drop
+# cd $class_repo_directory && git status
