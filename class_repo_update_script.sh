a6e883400945596269f94efa25b808c2fb102c9f
Sam Zun <zunsamuel97@gmail.com>
2023-07-14 16:08:08 -0500

    added section to set message based on pushing solutions

--- class_repo_update_script.sh
+++ class_repo_update_script.sh
@@ -2,30 +2,34 @@
 # Script for updating class repo
 ##########################################################
 
-# Config variables
-root_directory_path = '~/Documents/repos/UT-A-Data/'
+## Config variablesroot_directory
+root_directory = '~/Documents/repos/UT-A-Data/'
 class_repo_name = 'UTA-VIRT-DATA-PT-06-2023-U-LOLC/'
 master_repo_name = 'DataViz-Lesson-Plans/'
 lesson_directory = '01-Lesson-Plans/'
+homework_directory = '02-Homework/'
+canvas_directory = '04-Canvas/'
 week_directory = '06-Python-APIs/'
 day = '2'
 is_solutions = false
 
 
-# Path variables
+## Path variables
 lesson_week_directory = "${lesson_directory}${week_directory}"
 
-class_lesson_directory = "${root_directory_path}${class_repo_name}${lesson_week_directory}"
+class_repo_directory = "${root_directory}${class_repo_name}"
+class_lesson_directory = "${class_repo_directory}${lesson_week_directory}"
 class_lesson_day_directory = "${class_lesson_directory}${day}"
+class_homework_directory = "${class_repo_directory}${homework_directory}"
 
-master_repo_path = "${root_directory_path}${master_repo_name}"
-master_lesson_day_directory = "${master_repo_path}${lesson_week_directory}${day}"
+master_repo_directory = "${root_directory}${master_repo_name}"
+master_lesson_day_directory = "${master_repo_directory}${lesson_week_directory}${day}"
 
 
 ###### This section is to reset your class repo ######
 
 # Change directory into your local class repo
-cd "${root_directory_path}${class_repo_name}"
+cd "${root_directory}${class_repo_name}"
 
 # Pull current version of main
 git pull
@@ -36,6 +40,14 @@ git pull
 if [ ! -d "$class_lesson_directory" ]; then
     # Create lesson directory
     mkdir $class_lesson_directory
+
+    # Copy/Paste current week homework directory
+    cp -r "${master_repo_directory}${homework_directory}${week_directory}" $class_homework_directory
+    # Delete current week homework solutions
+    rm -rf "${class_homework_directory}${week_directory}Solutions"
+
+    # Copy/Paste current week Canvas directory
+    cp -r "${master_repo_directory}${canvas_directory}${week_directory}" "${class_repo_directort}${canvas_directory}${week_directory}"
 fi
 
 # Check if adding solutions
@@ -44,13 +56,13 @@ if [ "$is_solutions" = true ] ; then
     rm -rf "${class_lesson_day_directory}"
 fi
 
-# Copy folder
+# Copy/Paste folder
 cp -r $master_lesson_day_directory $class_lesson_directory
 
 # Check if not adding solutions
 if [ "$is_solutions" = false ] ; then
     # Delete student student solved folders
-    rm -rf "${class_lesson_day_directory}/Activities/**Stu**/Solved/"
+    rm -rf "${class_lesson_day_directory}/Activities/**Stu**/Solved"
 fi
 
 # Delete LessonPlan.md file
@@ -61,8 +73,13 @@ rm -rf "${class_lesson_day_directory}/LessonPlan.md"
 # Add changes
 git add -A
 
+# Set commit message
+message = "${week} Day ${day} content"
+if [ "$is_solutions" = true ] ; then
+    message = "${message} Solutions"
+fi
 # Commit changes
-git commit -m "${week} Day ${day} content"
+git commit -m "${message}"
 
 # Push changes
 git push -u origin main
@@ -70,7 +87,7 @@ git push -u origin main
 ###### This section is to reset the class repo ######
 
 # Change directory back into DataViz-Lesson-Plans
-cd "${master_repo_path}"
+cd "${master_repo_directory}"
 
 # Add changes
 git add -A
